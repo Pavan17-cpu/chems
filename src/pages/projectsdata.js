1dfc4efc59bad41d5efdb25b253f6e074ece5379
MANIKANTA SAI PAVAN <110300098+Pavan17-cpu@users.noreply.github.com>
2025-11-18 22:37:19 +0530

    Update projectsdata.js

--- src/pages/projectsdata.js
+++ src/pages/projectsdata.js
@@ -1,44 +1,44 @@
 const projectsdata = [
     {
-      title: "SHEYSHOP",
-      image: "./ecommerce.svg",
+      title: "Hydrochloric Acid - 33%",
+      image: "./hcl-33.jpg",
       description:
-        "An ecommerce application developed using react , node , redux and mongodb",
+        "Hydrochloric Acid 33% is a versatile industrial chemical, valued for its strength and reactivity. Itâ€™s essential in metal treatment, food processing, and chemical synthesis.",
+      link: "/contact",
+    },
+    {
+      title: "Bleaching Powder",
+      image: "./bleaching powder.jpg",
+      description:
+        "Bleaching Powder is a yellowishâ€‘white solid chemical, also known as Calcium Oxychloride or Calcium Hypochlorite, widely used as a disinfectant, cleaning agent, and bleaching agent.",
       link: "/",
     },
     {
-      title: "SHEYPIZZA",
-      image: "./pizzas.svg",
+      title: "Nitric Acid-60%",
+      image: "./nitric-acid-60.webp",
       description:
-        "An ecommerce application developed using react , node , redux and mongodb",
+        "Nitric Acid 60% is a strong mineral acid solution widely used in chemical industries, fertilizers, explosives, and metallurgy.",
       link: "/",
     },
     {
-      title: "SHEYROOMS",
-      image: "./hotels.svg",
+      title: "Sodium Hypochlorite",
+      image: "./sodium hypochlorite.png",
       description:
-        "An ecommerce application developed using react , node , redux and mongodb",
+        "Sodium Hypochlorite (NaOCl) is the active ingredient in household bleach, widely used as a disinfectant, sanitizer, and bleaching agent.",
       link: "/",
     },
     {
-      title: "SHEYJOBS",
-      image: "./jobs.svg",
+      title: "Soduim Silicate",
+      image: "./sodium silicate.jpg",
       description:
-        "An ecommerce application developed using react , node , redux and mongodb",
+        "Sodium Silicate (commonly called water glass or liquid glass) is a versatile inorganic chemical used in adhesives, detergents, construction, and water treatment.",
       link: "/",
     },
     {
-      title: "SHEYGRAM",
-      image: "./socialmedia.svg",
+      title: "Sulphuric Acid-98%",
+      image: "./sulphuric acid 98.webp",
       description:
-        "An ecommerce application developed using react , node , redux and mongodb",
-      link: "/",
-    },
-    {
-      title: "SHEYCARS",
-      image: "./cars.svg",
-      description:
-        "An ecommerce application developed using react , node , redux and mongodb",
+        "Sulphuric Acid 98% is a highly concentrated industrial acid, essential in fertilizers, petroleum refining, metallurgy, and battery production.",
       link: "/",
     },
   ];
