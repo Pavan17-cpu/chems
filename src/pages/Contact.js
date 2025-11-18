f2950148be04cfd75a425a238724eca363eb741c
MANIKANTA SAI PAVAN <110300098+Pavan17-cpu@users.noreply.github.com>
2025-11-18 22:35:06 +0530

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,29 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 function Contact() {
   return (
     <div>
-      <Header/>
-      <div className="container contact mt-5">
-        <div className="row pt-5">
-          <div className="col-md-6 p-5">
-              <img src="./contactus.svg" alt="" />
-          </div>
-          <div className="col-md-6">
-              <div className='contact-form m-2 p-5 n-box2'>
-                  <h3 className='font-bold'>Contact US</h3>
+      <Header />
 
-                  <hr />
-                  <input type="text" className='form-control' placeholder='Name'/>
-                  <input type="text" className='form-control' placeholder='Name'/>
+      <div>page</div>
 
-                  <textarea className="form-control" rows={3} defaultValue={""} />
-
-                  <button className='primary-button mt-3'>SUMBIT</button>
-              </div>
-          </div>
-        </div>
-      </div>
+      <br/>
+     
     </div>
   );
 }
